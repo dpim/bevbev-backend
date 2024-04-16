0bf3cb45a39dfdab998c147e29614f8d748e3cc2
Dmitry Pimenov <dmitryspimenov@gmail.com>
2024-04-15 22:20:08 -0700

    added middleware to try to identify user

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,16 +1,70 @@
 import express from 'express';
-import { getCachedOrFetch } from '../util/enrich.js';
+import geoip from 'geoip-lite';
+import moment from 'moment';
+import { getCachedOrFetch, VenueType } from '../util/enrich.js';
 const app = express();
+export const locationMiddleware = (req, res, next) => {
+    let ip = req.headers['x-forwarded-for'] || req.socket?.remoteAddress || '';
+    console.log(ip);
+    if (Array.isArray(ip)) {
+        ip = ip.length > 0 ? ip[0] : ''; // Take the first IP if it's an array
+    }
+    ip = ip.split(',')[0].trim(); // Now we safely call split      
+    const geo = geoip.lookup(ip);
+    if (geo) {
+        const [latitudeRaw, longitudeRaw] = geo.ll;
+        const latitude = `${latitudeRaw}`;
+        const longitude = `${longitudeRaw}`;
+        req.location = { latitude, longitude };
+    }
+    else {
+        req.location = null;
+    }
+    next();
+};
+const getHourFromISOTime = (isoTime) => {
+    // Parse the ISO time string with moment
+    const time = moment(isoTime);
+    // Use the .hour() method to get the hour component and return it
+    return time.hour(); // This returns the hour (0-23)
+};
+app.use(locationMiddleware);
 app.get("/test", async (req, res) => {
     res.send("Express on Vercel");
 });
 app.get("/v1/venues", async (req, res) => {
     try {
-        const { latitude, longitude, venue_type } = req.query;
-        if (!latitude || !longitude || !venue_type) {
-            return res.status(400).json({ error: 'Latitude, longitude, and venue_type are required parameters' });
+        let { latitude, longitude, venueType, isoTime } = req.query;
+        const dateHeader = req.get('Date');
+        let hour = null;
+        if (dateHeader) {
+            // Try to convert the Date header to a JavaScript Date object
+            hour = getHourFromISOTime(new Date(dateHeader).toISOString());
+            console.log(`Received a request with a Date header at ${hour} for ${req.originalUrl}`);
         }
-        const venues = await getCachedOrFetch(Number(latitude), Number(longitude), venue_type);
+        if (typeof isoTime === 'string') {
+            try {
+                hour = getHourFromISOTime(isoTime);
+            }
+            catch (error) {
+                res.status(400).send({ error: "Invalid ISO time format." });
+            }
+        }
+        else {
+            res.status(400).send({ error: "isoTime must be a string." });
+        }
+        // if not query params, try to derive
+        if (!latitude || !longitude) {
+            latitude = req.location?.latitude;
+            longitude = req.location?.longitude;
+        }
+        // if STILL not there, return an error
+        if (!latitude || !longitude || !hour) {
+            return res.status(400).json({ error: 'Location information or venue type is missing from the request.' });
+        }
+        venueType = venueType ?? (hour >= 5 && hour < 17 ? VenueType.coffee : VenueType.drinks);
+        // find venues based on metadata
+        const venues = await getCachedOrFetch(Number(latitude), Number(longitude), venueType);
         res.json(venues);
     }
     catch (error) {
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,20 +1,89 @@
 import express from 'express';
-import { Request, Response } from 'express';
+import geoip from 'geoip-lite';
+import moment from 'moment';
+import { Request, Response, NextFunction } from 'express';
 import { getCachedOrFetch, VenueType } from '../util/enrich.js';
 
 const app = express();
+
+interface CustomRequest extends Request {
+    location?: { latitude: string; longitude: string } | null;
+    venueType?: string;
+    isoTime?: string;
+}
+
+export const locationMiddleware = (req: CustomRequest, res: Response, next: NextFunction): void => {
+    let ip = req.headers['x-forwarded-for'] || req.socket?.remoteAddress || '';
+    console.log(ip)
+    if (Array.isArray(ip)) {
+        ip = ip.length > 0 ? ip[0] : '';  // Take the first IP if it's an array
+    }
+    ip = ip.split(',')[0].trim();  // Now we safely call split      
+    const geo = geoip.lookup(ip);
+
+    if (geo) {
+        const [latitudeRaw, longitudeRaw] = geo.ll;
+
+        const latitude = `${latitudeRaw}`;
+        const longitude = `${longitudeRaw}`;
+
+        req.location = { latitude, longitude };
+    } else {
+        req.location = null;
+    }
+    next();
+};
+
+const getHourFromISOTime = (isoTime: string): number => {
+    // Parse the ISO time string with moment
+    const time = moment(isoTime);
+
+    // Use the .hour() method to get the hour component and return it
+    return time.hour();  // This returns the hour (0-23)
+}
+
+app.use(locationMiddleware);
 
 app.get("/test", async (req: Request, res: Response) => {
     res.send("Express on Vercel");
 });
 
-app.get("/v1/venues", async (req: Request, res: Response) => {
+app.get("/v1/venues", async (req: CustomRequest, res: Response) => {
     try {
-        const { latitude, longitude, venue_type } = req.query;
-        if (!latitude || !longitude || !venue_type) {
-            return res.status(400).json({ error: 'Latitude, longitude, and venue_type are required parameters' });
+        let { latitude, longitude, venueType, isoTime } = req.query;
+        const dateHeader = req.get('Date');
+        let hour = null;
+
+        if (dateHeader) {
+            // Try to convert the Date header to a JavaScript Date object
+            hour = getHourFromISOTime(new Date(dateHeader).toISOString());
+            console.log(`Received a request with a Date header at ${hour} for ${req.originalUrl}`);
+        } 
+
+        if (typeof isoTime === 'string') {
+            try {
+                hour = getHourFromISOTime(isoTime);
+            } catch (error) {
+                res.status(400).send({ error: "Invalid ISO time format." });
+            }
+        } else {
+            res.status(400).send({ error: "isoTime must be a string." });
         }
-        const venues = await getCachedOrFetch(Number(latitude), Number(longitude), venue_type as VenueType);
+
+        // if not query params, try to derive
+        if (!latitude || !longitude) {
+            latitude = req.location?.latitude;
+            longitude = req.location?.longitude;
+        } 
+
+        // if STILL not there, return an error
+        if (!latitude || !longitude || !hour) {
+            return res.status(400).json({ error: 'Location information or venue type is missing from the request.' });
+        }
+
+        venueType = venueType ?? (hour >= 5 && hour < 17 ? VenueType.coffee : VenueType.drinks);
+        // find venues based on metadata
+        const venues = await getCachedOrFetch(Number(latitude), Number(longitude), venueType as VenueType);
         res.json(venues);
     } catch (error) {
         console.error("Error fetching venues:", error);
